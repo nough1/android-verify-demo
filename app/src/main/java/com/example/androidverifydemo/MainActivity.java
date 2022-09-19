453e2cb3979eaf80835724f4a54e17863f8878e0
guoanhu <8naouguh@gmail.com>
2022-09-20 07:40:29 +0800

    verify edit text and keyboard

--- app/src/main/java/com/example/androidverifydemo/MainActivity.java
+++ app/src/main/java/com/example/androidverifydemo/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.androidverifydemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        EditText editText = findViewById(R.id.edit2);
+        editText.requestFocus();
+        imm.showSoftInput(editText, InputMethodManager.SHOW_FORCED);
+
+        EditText editText1 = findViewById(R.id.edit1);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                editText1.requestFocus();
+
+            }
+        });
     }
+
+
 }
